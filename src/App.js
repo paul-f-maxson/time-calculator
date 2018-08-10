10b2b80c7da7f37de3c6a36d4527158e881fa7cd
Paul Maxson <paul.f.maxson@gmail.com>
2018-08-09 23:14:05 -0400

    All basic functionality complete, ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import dateFormat from 'dateformat';
 
 function Input(props) {
   return (
-    <h1>
-      ~~Input goes here~~
-    </h1>
+    <div className="input">
+      <h2>
+        <fieldset>
+          <legend>Enter start and end dates</legend>
+          <div>
+            <div className="startDateInput">
+              <label htmlFor="startDate">Start date/time:</label>
+              <input type="datetime-local" id="startDate"
+                name="startDate" value={props.startDate}
+                min="0000-00-00T00:00" max="9999-12-31T24:59"
+                onChange={props.onChange}
+              />
+            </div>
+            <div className="endDateInput">
+              <label htmlFor="endDate">End date/time:</label>
+              <input type="datetime-local" id="endDate"
+                name="endDate" value={props.endDate}
+                min="0000-00-00T00:00" max="9999-12-31T24:59"
+                onChange={props.onChange}
+              />
+            </div>
+          </div>
+        </fieldset>
+      </h2>
+    </div>
   );
 }
 
-// TODO: Plural Logic
-// TODO: Use a loop
 function Output(props) {
+  const amounts = (Object.keys(props.totals)).map((key) => {
+    let amount = props.totals[key];
+    return (
+      <li key={key}>{amount} {key}</li>
+    );
+  });
+
   return (
-    <div>
-      <h1>From {props.startDate}{" "}
+    <div className="output">
+      <h1 className="dateRange">
+        From {props.startDate}{" "}
         at {props.startTime}{" "}
         to {props.endDate}{" "}
-        at {props.endTime}, a total of:
-        <ul>
-          <li>{props.totals.secs} seconds elapsed</li>
-          <li>{props.totals.mins} minutes elapsed</li>
-          <li>{props.totals.hours} hours elapsed</li>
-          <li>{props.totals.days} days elapsed</li>
-          <li>{props.totals.months} months elapsed</li>
-          <li>{props.totals.years} years elapsed</li>
+        at {props.endTime}:
+        <ul className="amounts">
+          {amounts}
         </ul>
       </h1>
     </div>
@@ -36,43 +60,54 @@ class TimeCalculator extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      startDate: new Date("August 6, 2016 00:00:00"),
-      endDate: new Date("August 7, 2017 00:00:00"),
+      startDate: new Date(),
+      endDate: new Date(),
     };
 
     this.millis =  {
         year : 3.154*10**10, // Milliseconds in a year
-        month : 1.628*10**9, // Milliseconds in a month
+        month : 2.628*10**9, // Milliseconds in a month
         day : 8.64*10**7, // Milliseconds in a day
         hour : 3.60*10**6, // Milliseconds in an hour
-        min : 6.00*10**4, // Milliseconds in a minute
-        sec : 1000, // Milliseconds in a second
+        minute : 6.00*10**4, // Milliseconds in a minute
+        second : 1000, // Milliseconds in a second
     }
   }
 
-  render() {
-    // TODO: Use a loop
-    // TODO: Format date as Month dd, yyyy
-    const startDateFmt = this.state.startDate.toLocaleDateString();
-    const startTimeFmt = this.state.startDate.toLocaleTimeString();
-    const endDateFmt = this.state.endDate.toLocaleDateString();
-    const endTimeFmt = this.state.endDate.toLocaleTimeString();
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name
 
-    let diff = this.state.endDate.getTime() - this.state.startDate.getTime();
+    // TODO: convert from yyyy-mm-ddThh:mm
+    console.log(value);
+    this.setState({
+      [name]: new Date(value),
+    });
+  }
+
+  render() {
+    // TODO: implement more internationally accessible formatting
+    const startDateFmt = dateFormat(this.state.startDate, "longDate");
+    const startTimeFmt = dateFormat(this.state.startDate, "shortTime");
+    const endDateFmt = dateFormat(this.state.endDate, "longDate");
+    const endTimeFmt = dateFormat(this.state.startDate, "shortTime");
+
+    const diff = this.state.endDate.getTime() - this.state.startDate.getTime();
 
     let totals = {};
-    // TODO: Use a loop
-    totals.years = (diff / this.millis.year).toFixed(2);
-    totals.months = (diff / this.millis.month).toFixed(2);
-    totals.days = (diff / this.millis.day).toFixed(2);
-    totals.hours = (diff / this.millis.hour).toFixed(2);
-    totals.mins = (diff / this.millis.min).toFixed(2);
-    totals.secs = (diff / this.millis.sec).toFixed(2);
+
+    for (var increment in this.millis) {
+      totals[`${increment}s`] = (diff / this.millis[increment]).toFixed(2);
+    }
 
     return (
-      <div>
-        <Input />
-        <Output {}
+      <div className="timeCalculator">
+        <Input
+          // NOTE: Callback may need passed in a different format
+          onChange={(e) => this.handleChange(e)}
+        />
+        <Output
           // TODO: be more concise, use an object
           startDate={startDateFmt}
           startTime={startTimeFmt}
